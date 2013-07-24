c04a64432a9601d37cee124581af4cccac482065
Richard Bolt <richard@richardbolt.com>
2013-07-24 14:36:36 -0600

    Initial commit for the grunt-filerev-assets task.

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+/*
+ * grunt-filerev-assets
+ * https://github.com/rbolt/grunt-filerev-assets
+ *
+ * Copyright (c) 2013 Richard Bolt
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    filerev_assets: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!',
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
+        },
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'filerev_assets', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/filerev_assets.js
@@ -0,0 +1,71 @@
+/*
+ * grunt-filerev-assets
+ * https://github.com/richardbolt/grunt-filerev-assets
+ *
+ * Copyright (c) 2013 Richard Bolt
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+function stripPrefixFromObj(obj, options) {
+  var assets = {};
+  for (var _key in obj) {
+    if (obj.hasOwnProperty(_key)) {
+      var key = _key,
+          value = obj[key];
+      if (options.cwd) {
+        if (key.substr(0, options.cwd.length) === options.cwd) {
+            key = _key.substr(options.cwd.length);
+        }
+        if (obj[_key].substr(0, options.cwd.length) === options.cwd) {
+          value = obj[_key].substr(options.cwd.length);
+        }
+      }
+    assets[key] = value;
+    }
+  }
+    return assets;
+}
+
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('filerev_assets', 'Record asset paths from grunt-filerev to a json file', function() {
+      var self = this,
+        options = self.options({
+          dest: 'assets.json',  // Writes to this file.
+          cwd: ''  // Removes cwd from the paths recorded.
+        });
+
+      // We must have run filerev in some manner first.
+      // If we do this: grunt.task.requires('filerev');
+      // then if we ran filerev:action we will fail out,
+      // when we don't want to. This just checks for the presence of the
+      // grunt.filerev object and fails if it's not present.
+      // You can override the warning with the --force command line option.
+      if (!grunt.filerev) {
+        grunt.fail.warn('Could not find grunt.filerev. Required task "filerev" must be run first.');
+      }
+
+      if (!options.dest || !grunt.filerev.summary) {
+        grunt.log.error('No file saved.');
+        return;
+      }
+      
+      var assets;
+      if (options.cwd) {
+        assets = stripPrefixFromObj(grunt.filerev.summary, options);
+      } else {
+        assets = grunt.filerev.summary;
+      }
+
+      grunt.file.write(options.dest,
+                       JSON.stringify(assets));
+
+      grunt.filerevassets = assets;
+
+      grunt.log.writeln('File', options.dest, 'created.');
+  });
+
+};
